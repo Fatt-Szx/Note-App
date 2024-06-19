09593d926f216cc6032b11b0c97ed4f255bbf342
Fattah <fsyam67@gmail.com>
2024-06-19 16:17:56 +0700

    Develop add task feature

--- src/features/tasks/components/TaskProgress/TaskColumn.tsx
+++ src/features/tasks/components/TaskProgress/TaskColumn.tsx
@@ -1,43 +1,74 @@
-import React from 'react'
-import TaskCard from './TaskCard'
-import type { Task, CSSProperties } from '../../../../types'
+// components/TaskColumn.tsx
+import React, { useState } from 'react';
+import TaskCard from './TaskCard';
+import TaskModal from '../shared/TaskModal';
+import { TASK_MODAL_TYPE, TASK_PROGRESS_ID } from '../../../../constants/app';
+import type { Task } from '../../../../types';
 
 interface TaskColumnProps {
-  columnTitle: string
-  tasks: Task[]
+  columnTitle: string;
+  tasks: Task[];
+  progressOrder: number;
 }
 
-const TaskColumn = ({ columnTitle, tasks }: TaskColumnProps): JSX.Element => {
+const TaskColumn: React.FC<TaskColumnProps> = ({ columnTitle, tasks, progressOrder }) => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleOpenModal = (): void => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div style={styles.categoryColumn}>
       <div style={styles.columnTitleWrapper}>
         <h2 style={styles.categoryTitle}>{columnTitle}</h2>
-        <div className="material-icons" style={styles.plusIcon}>
-          add
-        </div>
+        <button style={styles.button} onClick={handleOpenModal}>
+          <span className="material-icons">add</span>
+        </button>
       </div>
       <div>
-        {tasks.map((task: Task) => {
-          return <TaskCard key={task.id} task={task} />
-        })}
+        {tasks.map((task: Task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
       </div>
+      {isModalOpen && (
+        <TaskModal
+          headingTitle={`Add ${columnTitle} Task`}
+          type={TASK_MODAL_TYPE.ADD}
+          setIsModalOpen={setIsModalOpen}
+          defaultProgressOrder={progressOrder}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-const styles: CSSProperties = {
-  plusIcon: {
-    cursor: 'pointer',
-  },
+const styles = {
   categoryColumn: {
-    width: '22%',
+    width: '300px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    padding: '10px',
+    marginBottom: '20px',
   },
   columnTitleWrapper: {
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '0 4px',
+    marginBottom: '10px',
   },
-}
+  categoryTitle: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    margin: '0',
+  },
+  button: {
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    border: 'none',
+    fontSize: '24px',
+    outline: 'none',
+  },
+};
 
-export default TaskColumn
+export default TaskColumn;
